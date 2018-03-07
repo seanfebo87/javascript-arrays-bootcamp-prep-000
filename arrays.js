27844094699ff527ee569aaabd757ba9f684eccd
Sean Febo <seanfebo@techie.com>
2018-03-07 01:50:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,3 +10,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index];
 }
+function destructivelyRemoveElementToBeginningOfArray(array, element) {
+ array.shift(element)
+ return array 
+}
