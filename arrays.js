fd72ded05f5b0d9c99375f21449c47f1c82a5852
Sean Febo <seanfebo@techie.com>
2018-03-07 01:53:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,3 +14,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element) {
  array.shift(element)
  return array 
 }
+function removeElementFromBeginningOfArray (array) {
+  return array.splice(1);
+}
