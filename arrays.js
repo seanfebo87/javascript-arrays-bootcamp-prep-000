a5681fcbcf1ef289badefc44f388280ecb40037b
Sean Febo <seanfebo@techie.com>
2018-03-07 02:09:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,4 +27,7 @@ function removeElementFromEndOfArray(array) {
 function addElementToEndOfArray (array, element) {
   return [...array, element];
 }
-function 
+function destructivelyAddElementToEndOfArray (array, element) {
+  array.push(element)
+  return array
+}
